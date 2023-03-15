49b95fecfac0688c55441757f8e94c73e07f7a6a
Enoc Sibaja Carrión <86577450+esibaja0795@users.noreply.github.com>
2023-03-15 03:03:13 -0600

    Add files via upload

---
+++ proyecto_programado1_Enoc_Sibaja/src/Main.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Main {
+    ListaVagones lista = new ListaVagones();
+    String tipoFruta = "";
+    int kilos = 0, ciudad = 0, numeroVagon = 0, kilosDescarga = 0;
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner ent = new Scanner(System.in);
+
+        System.out.println("Bienvenido a la estacion de carga/descarga de frutas de la ciudad: ");
+        System.out.println("Ingrese numero segun requiera: ");
+        System.out.println(">> Cargar fruta: 1 << || >> Descargar fruta: 2 << || >> SALIR: 0 <<");
+        int opcion = ent.nextInt();
+
+        while (opcion > 0 && main.ciudad < 10) {
+            if (opcion == 1) {
+                main.cargar();
+                System.out.println("Bienvenido a la estacion de carga/descarga de frutas de la ciudad: ");
+                System.out.println("Ingrese numero segun requiera: ");
+                System.out.println(">> Cargar fruta: 1 << || >> Descargar fruta: 2 << || >> SALIR: 0 <<");
+                opcion = ent.nextInt();
+            } else {
+                main.descargar();
+                System.out.println("Bienvenido a la estacion de carga/descarga de frutas de la ciudad: ");
+                System.out.println("Ingrese numero segun requiera: ");
+                System.out.println(">> Cargar fruta: 1 << || >> Descargar fruta: 2 << || >> SALIR: 0 <<");
+                opcion = ent.nextInt();
+            }
+        }
+    }
+
+    public void cargar() {
+        Scanner ent = new Scanner(System.in);
+        Fruta fruta = new Fruta(numeroVagon, ciudad, tipoFruta, kilos);
+
+        System.out.println("Ingrese tipo de fruta a cargar: ");
+        tipoFruta = ent.nextLine();
+        System.out.println("Ingrese cantidad de kilos de carga:");
+        kilos = ent.nextInt();
+
+        ciudad = fruta.getCiudad() + 1;
+        numeroVagon = fruta.getNumeroVagon() + 1;
+
+        fruta.setCiudad(ciudad);
+        fruta.setNumeroVagon(numeroVagon);
+        fruta.setKilos(kilos);
+        fruta.setTipoFruta(tipoFruta);
+        if(fruta.getNumeroVagon() == 0) {
+            lista.insertarPrimero(fruta);
+        } else {
+            lista.insertarFinal(fruta);
+        }
+        lista.mostrarData(fruta);
+    }
+
+    public void descargar() {
+        Scanner ent = new Scanner(System.in);
+        Fruta fruta = new Fruta(numeroVagon, ciudad, tipoFruta, kilos);
+
+        System.out.println("Ingrese numero de vagon a descargar: ");
+        numeroVagon = ent.nextInt();
+        System.out.println("Ingrese cantidad de kilos de descarga:");
+        kilosDescarga = ent.nextInt();
+
+        lista.descargarVagon(numeroVagon, fruta, kilosDescarga);
+        lista.mostrarData(fruta);
+    }
+}
